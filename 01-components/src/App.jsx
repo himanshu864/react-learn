e2b697472e8a724d1f2c4506d285d979cc928b88
himanshu864 <himanshu864000@gmail.com>
2024-05-28 16:49:57 +0530

    Clean up; components

--- 01-components/src/App.jsx
+++ 01-components/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const dudes = ["Himanshu", "Malik", "Angel"];
+function getRandIdx(size) {
+  return Math.floor(Math.random() * size);
 }
 
-export default App
+function Extra() {
+  const name = dudes[getRandIdx(dudes.length)];
+  return (
+    <div>
+      <h3>My name is {name}</h3>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, provident?
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <h1>Hello Moto</h1>
+      <Extra />
+    </>
+  );
+}
