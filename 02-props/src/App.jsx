64a88bae062c341090a0414341211556c1b2a1ef
himanshu864 <himanshu864000@gmail.com>
2024-05-29 17:03:52 +0530

    Events and Configure Handlers

--- 02-props/src/App.jsx
+++ 02-props/src/App.jsx
@@ -8,6 +8,17 @@ import {
 import TabButton from "./components/TabButton.jsx";
 
 export default function App() {
+  function handleSelect(tab) {
+    if (tab == 1) {
+      console.log("Components");
+    } else if (tab == 2) {
+      console.log("JSX");
+    } else if (tab == 3) {
+      console.log("Props");
+    } else {
+      console.log("State");
+    }
+  }
   return (
     <div>
       <Header />
@@ -29,17 +40,17 @@ export default function App() {
             <Fundamental>
               <img src={CORE_CONCEPTS[3].image} alt="Cool image" />
               <h3>{CORE_CONCEPTS[3].title}</h3>
-              <p>{CORE_CONCEPTS[3].description}</p>
+              <p>{CORE_CONCEPTS[3].descriphandleSelecttion}</p>
             </Fundamental>
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Component</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect(1)}>Component</TabButton>
+            <TabButton onSelect={() => handleSelect(2)}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect(3)}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect(4)}>State</TabButton>
           </menu>
         </section>
       </main>
