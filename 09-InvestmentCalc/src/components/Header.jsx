a2166ba72b63f7f347ce531d2b42b29c4016434e
himanshu864 <himanshu864000@gmail.com>
2024-06-05 20:54:24 +0530

    Investment Calc Start

---
+++ 09-InvestmentCalc/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header id="header">
+      <img src="./investment-calculator-logo.png" alt="money bag image" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+}
