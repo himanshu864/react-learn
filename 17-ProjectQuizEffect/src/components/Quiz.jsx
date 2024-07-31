605907164c25188bef8add04797f0b19c72feb94
himanshu864 <himanshu96864@gmail.com>
2024-07-31 18:23:11 +0530

    major : I am a goddamn genius

--- 17-ProjectQuizEffect/src/components/Quiz.jsx
+++ 17-ProjectQuizEffect/src/components/Quiz.jsx
@@ -1,28 +1,75 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import questions from "../questions";
-import Progress from "./Progress";
+import answerKey from "../answerKey";
+
+const TIMER = 5000;
+const PAUSE = 2000;
 
 export default function Quiz({ qn, onSelect }) {
-  const [trigger, setTrigger] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(TIMER);
+  const [isPaused, setPause] = useState(false);
+  const [selected, setSelected] = useState(-1);
 
-  const handleSelect = (i) => {
-    onSelect(i);
-    setTrigger((prev) => prev + 1);
-  };
+  // when user select or skip
+  const handleSelect = useCallback((i) => {
+    setPause(true);
+    setTimeLeft(PAUSE);
+    setSelected(i);
+  }, []);
+
+  // time keeps on running
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft((left) => left - 10);
+    }, 10);
+    return () => clearInterval(interval);
+  }, []);
+
+  // time up
+  useEffect(() => {
+    if (timeLeft === 0) {
+      if (!isPaused) handleSelect(-1);
+      else {
+        setPause(false);
+        setTimeLeft(TIMER);
+        onSelect(selected);
+      }
+    }
+  }, [timeLeft, handleSelect, onSelect, selected, isPaused]);
+
+  const firstHalf = 2 * timeLeft > PAUSE;
 
   return (
     <div id="quiz">
       <div id="question">
-        <Progress onSelect={onSelect} trigger={trigger} />
+        <progress
+          className={isPaused && firstHalf ? "answered" : undefined}
+          value={isPaused && firstHalf ? timeLeft - PAUSE / 2 : timeLeft}
+          max={isPaused ? PAUSE / 2 : TIMER}
+        />
         <h2>{questions[qn].text}</h2>
       </div>
 
       <div id="answers">
-        {questions[qn].answers.map((ans, i) => (
-          <div className="answer" key={i}>
-            <button onClick={() => handleSelect(i)}>{ans} </button>
-          </div>
-        ))}
+        {questions[qn].answers.map((ans, i) => {
+          let classes = "";
+          if (i === selected && isPaused) {
+            if (firstHalf) classes = "selected";
+            else classes = answerKey[qn] === i ? "correct" : "wrong";
+          }
+
+          return (
+            <div className="answer" key={i}>
+              <button
+                onClick={() => handleSelect(i)}
+                className={classes}
+                disabled={isPaused}
+              >
+                {ans}
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
